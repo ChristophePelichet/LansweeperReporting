086963c3e88d6a164ef817506fa996745742d045
Christophe Pelichet <47081682+ChristophePelichet@users.noreply.github.com>
2021-02-19 09:13:12 +0100

    Update Query

--- Query/query_count_kas_client.sql
+++ Query/query_count_kas_client.sql
@@ -4,9 +4,20 @@ SELECT COUNT(*)
 		Inner Join tblSoftwareUni On tblSoftware.softID = tblSoftwareUni.SoftID 
 	
 	WHERE tblSoftwareUni.SoftwarePublisher = 'AO Kaspersky Lab'
-			AND tblSoftwareUni.SoftwareName = 'Kaspersky Endpoint Security for Windows'
+			AND (tblSoftwareUni.SoftwareName = 'Kaspersky Endpoint Security for Windows'
+				OR tblSoftwareUni.SoftwareName LIKE 'Kaspersky Endpoint Agent%')
 			AND tblAssets.scanserver = 'scannerToChange' 
 
 
+-- In this query we count only the KAS client for desktop or laptop
 
-			
+
+-- On this query we select only Kaspersky Endpoint Security for Windows or Kaspersky Endpoint Agent
+-- If you want to show the all Kaspersky Publisher Software in your DB you can use this query directly.
+
+-- SELECT	tblSoftwareUni.SoftwarePublisher,
+--			tblSoftwareUni.SoftwareName
+--	From tblSoftware 
+--		Inner Join tblAssets On tblSoftware.AssetID = tblAssets.AssetID 
+--		Inner Join tblSoftwareUni On tblSoftware.softID = tblSoftwareUni.SoftID		
+--	WHERE tblSoftwareUni.SoftwarePublisher = 'AO Kaspersky Lab'
