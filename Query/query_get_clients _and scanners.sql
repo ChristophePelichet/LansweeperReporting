78fdc1b33daa6fac7b45119e46e5ad589bd3c240
Christophe Pelichet <47081682+ChristophePelichet@users.noreply.github.com>
2021-02-19 08:46:21 +0100

    Add Alphabetical Order

--- Query/query_get_clients _and scanners.sql
+++ Query/query_get_clients _and scanners.sql
@@ -4,3 +4,5 @@ SELECT	tblAssetGroups.AssetGroup,
 FROM tblAssetGroups 
 
 INNER JOIN tsysAssetGroupFilter On tblAssetGroups.AssetGroupID = tsysAssetGroupFilter.AssetGroupID 
+
+ORDER BY tblAssetGroups.AssetGroup ASC;
